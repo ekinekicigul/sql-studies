970ce8feacd2a90ecb957e0a83c3ae830e4de02a
ekinekicigul <133028025+ekinekicigul@users.noreply.github.com>
2025-02-24 14:51:41 +0300

    Create KurumsalCalisanlar.sql

---
+++ Basic/KurumsalCalisanlar.sql
@@ -0,0 +1,26 @@
+CREATE TABLE KurumsalCalısanlar
+(
+ID INT,
+Ad varchar(30),
+Cinsiyet varchar(30),
+Maas INT,
+Ulke varchar(50),
+DT DATE,
+İşe_Giriş_Tarihi DATE
+);
+INSERT INTO KurumsalCalısanlar VALUES
+(1,'Onur','Erkek',3000,'Türkiye','1985-04-16','2012-04-20'),
+(2,'Murat','Erkek',4000,'Fransa','1990-09-16','2004-04-20'),
+(3,'Kemal','Erkek',5000,'Türkiye','2000-04-25','2008-04-20'),
+(4,'Mehmet','Erkek',6000,'Rusya','2002-09-16','2012-04-20'),
+(5,'Ezgi','Kadın',7000,'Rusya','1985-08-16','2012-04-20'),
+(6,'Ayşe','Kadın',8000,'Estonya','1985-07-16','2012-04-20'),
+(7,'Sema','Kadın',9000,'Türkiye','1996-12-16','2020-04-20'),
+(8,'Buket','Kadın',3000,'Amerika','1985-12-16','2012-04-20'),
+(9,'Fatma','Kadın',3000,'Fransa','1995-03-16','2024-04-20'),
+(10,'Esin','Kadın',3000,'Amerika','1985-04-16','2012-04-20'),
+(11,'Onur','Erkek',5000,'Türkiye','1985-09-16','2012-04-20'),
+(12,'Onur','Erkek',5000,'Fransa','1985-04-16','2021-04-20'),
+(13,'Onur','Erkek',5000,'Fransa','1993-08-16','2012-04-20'),
+(14,'Onur','Erkek',5000,'Amerika','1985-04-16','2012-04-20'),
+(15,'Mehmet','Erkek',5000,'Rusya','1985-04-16','2012-04-20');
